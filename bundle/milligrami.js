91f9cf54a2fe7ec77eb88a7f7aa41f98cbfd9944
D10221 <danieliozzi@gmail.com>
2017-06-09 07:12:59 +0200

    bundle

--- bundle/milligrami.js
+++ bundle/milligrami.js
@@ -73,7 +73,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 10);
+/******/ 	return __webpack_require__(__webpack_require__.s = 11);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -127,25 +127,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
 var is_defined_1 = __webpack_require__(1);
 exports.Button = function (xProps) {
-    var outline = xProps.outline, clear = xProps.clear, props = __rest(xProps, ["outline", "clear"]);
-    var className = "button" +
+    var outline = xProps.outline, clear = xProps.clear, className = xProps.className, props = __rest(xProps, ["outline", "clear", "className"]);
+    var cn = (className ? className + " " : "") + "button" +
         (is_defined_1.isDefined(outline) && outline !== false ? " button-outline" : "") +
         (is_defined_1.isDefined(clear) && clear !== false ? " button-clear" : "");
-    return (React.createElement("button", __assign({}, __assign({ className: className }, props))));
+    return (React.createElement("button", __assign({}, __assign({ className: cn }, props))));
 };
 exports.LinkButton = function (xProps) {
-    var outline = xProps.outline, clear = xProps.clear, props = __rest(xProps, ["outline", "clear"]);
-    var className = "button" +
+    var outline = xProps.outline, clear = xProps.clear, className = xProps.className, props = __rest(xProps, ["outline", "clear", "className"]);
+    var cn = (className ? className + " " : "") + "button" +
         (is_defined_1.isDefined(outline) && outline !== false ? " button-outline" : "") +
         (is_defined_1.isDefined(clear) && clear !== false ? " button-clear" : "");
-    return (React.createElement("a", __assign({}, __assign({ className: className }, props))));
+    return (React.createElement("a", __assign({}, __assign({ className: cn }, props))));
 };
 exports.InputButton = function (xProps) {
-    var outline = xProps.outline, clear = xProps.clear, props = __rest(xProps, ["outline", "clear"]);
-    var className = "button" +
+    var outline = xProps.outline, clear = xProps.clear, className = xProps.className, props = __rest(xProps, ["outline", "clear", "className"]);
+    var cn = (className ? className + " " : "") + "button" +
         (is_defined_1.isDefined(outline) && outline !== false ? " button-outline" : "") +
         (is_defined_1.isDefined(clear) && clear !== false ? " button-clear" : "");
-    return (React.createElement("input", __assign({}, __assign({ className: className }, props))));
+    return (React.createElement("input", __assign({}, __assign({ className: cn }, props))));
 };
 
 
@@ -176,39 +176,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
 /**
  * Clear any float
+ * TODO: HOC
  */
 exports.ClearFix = function (xProps) {
     var className = xProps.className, props = __rest(xProps, ["className"]);
-    var augmented = className + " clearfix";
+    var augmented = (className ? className + " " : "") + " clearfix";
     return (React.createElement("div", __assign({}, __assign({ className: augmented }, props))));
 };
 
 
 /***/ }),
 /* 4 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-var __assign = (this && this.__assign) || Object.assign || function(t) {
-    for (var s, i = 1, n = arguments.length; i < n; i++) {
-        s = arguments[i];
-        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-            t[p] = s[p];
-    }
-    return t;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = __webpack_require__(0);
-/**
- * Element ?
- */
-exports.Codes = function (props) { return (React.createElement("pre", null,
-    React.createElement("code", __assign({}, props)))); };
-
-
-/***/ }),
-/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -255,7 +233,7 @@ exports.ColumnSpan = function (xProps) {
 
 
 /***/ }),
-/* 6 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -281,13 +259,56 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
 exports.Container = function (xProps) {
     var className = xProps.className, props = __rest(xProps, ["className"]);
-    var augmented = "container " + className;
-    return (React.createElement("div", __assign({}, __assign({ augmented: augmented }, props))));
+    var augmented = (className ? className + " " : "") + "container";
+    return (React.createElement("div", __assign({}, __assign({ className: augmented }, props))));
 };
 exports.ContainerSection = function (xProps) {
     var className = xProps.className, props = __rest(xProps, ["className"]);
-    var augmented = "container " + className;
-    return (React.createElement("section", __assign({}, __assign({ augmented: augmented }, props))));
+    var augmented = (className ? className + " " : "") + "container";
+    return (React.createElement("section", __assign({}, __assign({ className: augmented }, props))));
+};
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = __webpack_require__(0);
+var warn_1 = __webpack_require__(12);
+/**
+ * TODO: HOC
+ */
+exports.Float = function (xProps) {
+    var left = xProps.left, right = xProps.right, direction = xProps.direction, className = xProps.className, props = __rest(xProps, ["left", "right", "direction", "className"]);
+    var cname = (className ? className + " " : "") +
+        (left ? "float-left" : "") +
+        (right ? "float-right" : "") +
+        (direction === "left" ? "float-left" : "") +
+        (direction === "right" ? "float-right" : "");
+    if (left && right || ((left || right) && direction)) {
+        warn_1.warn("what are you trying to do ? " + cname);
+    }
+    return (React.createElement("div", __assign({}, __assign({ className: cname }, props))));
 };
 
 
@@ -316,13 +337,12 @@ var __rest = (this && this.__rest) || function (s, e) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
-exports.Float = function (xProps) {
-    var xFloat = xProps.xFloat, className = xProps.className, props = __rest(xProps, ["xFloat", "className"]);
-    var cname = className +
-        (xFloat === "left"
-            ? " float-left"
-            : " float-right");
-    return (React.createElement("div", __assign({}, __assign({ className: cname }, props))));
+var is_defined_1 = __webpack_require__(1);
+exports.Label = function (xProps) {
+    var inLine = xProps.inLine, className = xProps.className, props = __rest(xProps, ["inLine", "className"]);
+    var augmented = (className ? className + " " : "") +
+        (is_defined_1.isUndefinedOrFalse(inLine) ? "" : "label-inline");
+    return (React.createElement("label", __assign({}, __assign({ className: augmented }, props))));
 };
 
 
@@ -340,24 +360,13 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
     }
     return t;
 };
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
-            t[p[i]] = s[p[i]];
-    return t;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
-var is_defined_1 = __webpack_require__(1);
-exports.Label = function (xProps) {
-    var inLine = xProps.inLine, className = xProps.className, props = __rest(xProps, ["inLine", "className"]);
-    var augmented = className +
-        (is_defined_1.isUndefinedOrFalse(inLine) ? "" : " label-inline");
-    return (React.createElement("label", __assign({}, __assign({ className: augmented }, props))));
-};
+/**
+ * Element ?
+ */
+exports.PreCode = function (props) { return (React.createElement("pre", null,
+    React.createElement("code", __assign({}, props)))); };
 
 
 /***/ }),
@@ -390,21 +399,211 @@ var is_defined_1 = __webpack_require__(1);
  *
  */
 exports.Row = function (xProps) {
-    var rowWrap = xProps.rowWrap, rowNoPadding = xProps.rowNoPadding, rowTop = xProps.rowTop, rowBottom = xProps.rowBottom, rowCenter = xProps.rowCenter, rowStretch = xProps.rowStretch, rowBaseline = xProps.rowBaseline, props = __rest(xProps, ["rowWrap", "rowNoPadding", "rowTop", "rowBottom", "rowCenter", "rowStretch", "rowBaseline"]);
-    var className = "row" +
+    var rowWrap = xProps.rowWrap, rowNoPadding = xProps.rowNoPadding, rowTop = xProps.rowTop, rowBottom = xProps.rowBottom, rowCenter = xProps.rowCenter, rowStretch = xProps.rowStretch, rowBaseline = xProps.rowBaseline, className = xProps.className, props = __rest(xProps, ["rowWrap", "rowNoPadding", "rowTop", "rowBottom", "rowCenter", "rowStretch", "rowBaseline", "className"]);
+    var cn = (className ? className + " " : "") + "row" +
         (is_defined_1.isUndefinedOrFalse(rowWrap) ? "" : " row-wrap") +
         (is_defined_1.isUndefinedOrFalse(rowNoPadding) ? "" : " row-no-padding") +
         (is_defined_1.isUndefinedOrFalse(rowTop) ? "" : " row-top") +
         (is_defined_1.isUndefinedOrFalse(rowBottom) ? "" : " row-bottom") +
         (is_defined_1.isUndefinedOrFalse(rowCenter) ? "" : " row-center") +
         (is_defined_1.isUndefinedOrFalse(rowStretch) ? "" : " row-stretch") +
-        (is_defined_1.isUndefinedOrFalse(rowStretch) ? "" : " row-baseline");
-    return (React.createElement("div", __assign({}, __assign({ className: className }, props))));
+        (is_defined_1.isUndefinedOrFalse(rowBaseline) ? "" : " row-baseline");
+    return (React.createElement("div", __assign({}, __assign({ className: cn }, props))));
 };
 
 
 /***/ }),
 /* 10 */
+/***/ (function(module, exports) {
+
+// shim for using process in browser
+var process = module.exports = {};
+
+// cached from whatever global is present so that test runners that stub it
+// don't break things.  But we need to wrap it in a try catch in case it is
+// wrapped in strict mode code which doesn't define any globals.  It's inside a
+// function because try/catches deoptimize in certain engines.
+
+var cachedSetTimeout;
+var cachedClearTimeout;
+
+function defaultSetTimout() {
+    throw new Error('setTimeout has not been defined');
+}
+function defaultClearTimeout () {
+    throw new Error('clearTimeout has not been defined');
+}
+(function () {
+    try {
+        if (typeof setTimeout === 'function') {
+            cachedSetTimeout = setTimeout;
+        } else {
+            cachedSetTimeout = defaultSetTimout;
+        }
+    } catch (e) {
+        cachedSetTimeout = defaultSetTimout;
+    }
+    try {
+        if (typeof clearTimeout === 'function') {
+            cachedClearTimeout = clearTimeout;
+        } else {
+            cachedClearTimeout = defaultClearTimeout;
+        }
+    } catch (e) {
+        cachedClearTimeout = defaultClearTimeout;
+    }
+} ())
+function runTimeout(fun) {
+    if (cachedSetTimeout === setTimeout) {
+        //normal enviroments in sane situations
+        return setTimeout(fun, 0);
+    }
+    // if setTimeout wasn't available but was latter defined
+    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+        cachedSetTimeout = setTimeout;
+        return setTimeout(fun, 0);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedSetTimeout(fun, 0);
+    } catch(e){
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+            return cachedSetTimeout.call(null, fun, 0);
+        } catch(e){
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+            return cachedSetTimeout.call(this, fun, 0);
+        }
+    }
+
+
+}
+function runClearTimeout(marker) {
+    if (cachedClearTimeout === clearTimeout) {
+        //normal enviroments in sane situations
+        return clearTimeout(marker);
+    }
+    // if clearTimeout wasn't available but was latter defined
+    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+        cachedClearTimeout = clearTimeout;
+        return clearTimeout(marker);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedClearTimeout(marker);
+    } catch (e){
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+            return cachedClearTimeout.call(null, marker);
+        } catch (e){
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+            return cachedClearTimeout.call(this, marker);
+        }
+    }
+
+
+
+}
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = runTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            if (currentQueue) {
+                currentQueue[queueIndex].run();
+            }
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    runClearTimeout(timeout);
+}
+
+process.nextTick = function (fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        runTimeout(drainQueue);
+    }
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+process.prependListener = noop;
+process.prependOnceListener = noop;
+
+process.listeners = function (name) { return [] }
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+};
+
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+process.umask = function() { return 0; };
+
+
+/***/ }),
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -418,13 +617,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 __export(__webpack_require__(2));
 __export(__webpack_require__(3));
+__export(__webpack_require__(8));
 __export(__webpack_require__(4));
 __export(__webpack_require__(5));
 __export(__webpack_require__(6));
 __export(__webpack_require__(7));
-__export(__webpack_require__(8));
 __export(__webpack_require__(9));
 
+
+/***/ }),
+/* 12 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(process) {
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.warn = function () {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test") {
+        console.warn(args);
+    }
+};
+
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(10)))
 
 /***/ })
 /******/ ]);
