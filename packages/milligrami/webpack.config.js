5f012e4f7f857a4ce8fa180f87c9505aec06af6c
Daniel <danieliozzi@gmail.com>
2018-11-14 19:40:28 +0200

    back to typescript

--- packages/milligrami/webpack.config.js
+++ packages/milligrami/webpack.config.js
@@ -1,41 +1,36 @@
 const path = require("path");
-const webpack = require("webpack");
-const tsConfigPath = path.resolve(__dirname, "./tsconfig.es5.json")
-const args = process.argv.slice(2);
-const mode = args[args.indexOf("--mode") + 1] === "production" ? "production" : "development";
-const production = mode === "production";
-console.log("mode: production: ", mode, production);
-module.exports = {
-    devtool: (!production ? "source-map" : undefined), 
-    mode,
-    entry: {
-        ['milligrami' + (production ? ".min" : "")]: path.resolve(__dirname, 'src/index.ts'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'lib/umd'),
-        filename: '[name].js',
-        libraryTarget: 'umd',
-        library: 'Milligrami',
-        umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', 'js', 'jsx']
-    },
-    // devtool: 'source-map',
-    plugins: [
-
+const tsConfigPath = path.resolve(__dirname, "./tsconfig.es5.json");
+module.exports = {  
+  mode: "production",
+  entry: {
+    ["milligrami.min"]: path.resolve(
+      __dirname,
+      "src/index.ts",
+    ),
+  },
+  output: {
+    path: path.resolve(__dirname, "lib/umd"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "Milligrami",
+    umdNamedDefine: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", "js", "jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          configFile: tsConfigPath,
+        },
+      },
     ],
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: 'ts-loader',
-            options: {
-                configFile: tsConfigPath
-            }
-        }],
-    },
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    }
-}
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+};
